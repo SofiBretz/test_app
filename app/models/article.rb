385fd93131a8e5fdf99a47f54c15cf9c3fe24c75
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-07 20:53:31 -0300

    addeda save place for the form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Article < ApplicationRecord
-  validates :title, presence: true, length :{minimun: 3, maximium: 50}
-  validates :description, presence: true, length :{minimun: 3, maximium: 300}
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 3, maximium: 50}
+  validates :description, presence: true, length: {minimum: 3, maximium: 300}
 end
