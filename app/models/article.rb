ae44374fee0c1da74acd4b93d5485a92a31dfb82
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-07 19:33:48 -0300

    created a new article page and with a form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length :{minimun: 3, maximium: 50}
+  validates :description, presence: true, length :{minimun: 3, maximium: 300}
 end
