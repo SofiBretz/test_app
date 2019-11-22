d69c8562ca5e8552cf058ca862aca5f215ef3e44
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-21 21:37:54 -0300

    added navigataion partial and custom css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require rails_ujs
+//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
