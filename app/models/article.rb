eda835bca29ae58f56332f87b4dce0ae9449631e
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-12-18 21:39:44 -0300

    added associtaion between user and articles, added debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximium: 50}
   validates :description, presence: true, length: {minimum: 3, maximium: 300}
 end
