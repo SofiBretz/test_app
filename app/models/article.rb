1d7ae5be428cbadbd0410c6b15de723ab2a632da
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-10-27 23:50:10 -0600

    added an optional true to the belongs user on the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :title, presence: true, length: {minimum: 3, maximium: 50}
   validates :description, presence: true, length: {minimum: 3, maximium: 300}
 end
